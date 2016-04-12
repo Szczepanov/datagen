c7146bb5fdd4b6c1b2caffe02d9037dab87501a6
Szczepanov <mdszczepanski@gmail.com>
2016-04-12 13:18:28 +0200

    dodanie gitignore oraz zmiany w settings

--- datagen/settings.py
+++ datagen/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'generator',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -74,23 +75,23 @@ WSGI_APPLICATION = 'datagen.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.oracle',
-        'NAME': 'hr',
-        'USER': 'hr',
-        'PASSWORD': 'hr',
-        'HOST': '127.0.0.1',
-        'PORT': '1521',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.oracle',
+#        'NAME': 'hr',
+#        'USER': 'hr',
+#        'PASSWORD': 'hr',
+#        'HOST': '127.0.0.1',
+#        'PORT': '1521',
+#    }
+#}
 
 
 # Password validation
