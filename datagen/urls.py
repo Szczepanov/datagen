322f21fbc2270a5edb0323ec95271e11f7846eea
Szczepanov <mdszczepanski@gmail.com>
2016-04-13 16:31:06 +0200

    added DataSet model and some methods (init, generate, str)

--- datagen/urls.py
+++ datagen/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 admin.autodiscover()
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', ),
 ]
--- generator/models.py
+++ generator/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class DataSet(models.Model):
+    name = models.TextField()
+    country = models.TextField()
+    columnName = models.TextField()
+    dataType = models.TextField()
+    rowsNumber = models.TextField()
+
+    def __init__(self):
+        self.name = ''
+        self.country = 'All'
+        self.columnName = ''
+        self.rowsNumber = '1'
+
+    def generate(self):
+        self.save()
+
+    def __str__(self):
+        return self.name
